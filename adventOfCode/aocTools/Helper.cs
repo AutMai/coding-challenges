62ab36ba11a891ffa596effde5ae85394e67dbf9
Sebastian <shuber0802@gmail.com>
2021-12-15 08:21:59 +0100

    fixed Helper

--- adventOfCode/aocTools/Helper.cs
+++ adventOfCode/aocTools/Helper.cs
@@ -4,5 +4,5 @@ public static class Helper {
     private static readonly string WorkingDirectory = Environment.CurrentDirectory;
 
     public static string ReadFile(string filename) =>
-        Helper.ReadFile("input.txt");
+        File.ReadAllText(Path.Combine(Directory.GetParent(WorkingDirectory).Parent.Parent.FullName, filename)).Replace("\r","");
 }
