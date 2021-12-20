8e9f6478c2f87d990fdead223885011cfa533d9f
Sebastian Huber <shuber0802@gmail.com>
2021-12-20 16:50:54 +0100

    + day19

---
+++ adventOfCode/day19/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+using aocTools;
+using day19;
+
+var input = Helper.ReadFile("input_small.txt");
+
+var scannerReports = Regex.Split(input, @"--- scanner \d+ ---");
+scannerReports = scannerReports.Where(s => s != String.Empty).ToArray();
+
+var scanners = new List<Scanner>();
+
+for (int i = 0; i < scannerReports.Length; i++) {
+    var s = new Scanner(i);
+    scanners.Add(s);
+    var coords = scannerReports[i].Split('\n', StringSplitOptions.RemoveEmptyEntries);
+    foreach (var coord in coords) {
+        var coordParts = Array.ConvertAll(coord.Split(',', StringSplitOptions.RemoveEmptyEntries), float.Parse);
+        var v = new Vector3(coordParts[0], coordParts[1], coordParts[2]);
+        s.Beacons.Add(v);
+    }
+}
+
+var scanner0 = new Vector3(0, 0, 0);
+
+var distances = new Dictionary<(Vector3 b1, Vector3 b2), float>();
+var distances2 = new Dictionary<(Vector3 b1, Vector3 b2), float>();
+
+
+foreach (var b1 in scanners[0].Beacons)
+foreach (var b2 in scanners[0].Beacons) {
+    if (Vector3.Distance(b1, b2) != 0 && !distances.ContainsValue(Vector3.Distance(b1, b2)))
+        distances[(b1, b2)] = (Vector3.Distance(b1, b2));
+}
+
+
+foreach (var b1 in scanners[1].Beacons)
+foreach (var b2 in scanners[1].Beacons) {
+    if (Vector3.Distance(b1, b2) != 0 && !distances2.ContainsValue(Vector3.Distance(b1, b2)))
+        distances2[(b1, b2)] = (Vector3.Distance(b1, b2));
+}
+
+foreach (var d in distances)
+foreach (var d2 in distances2) {
+    if (d.Value == d2.Value) Console.WriteLine(d.Key + " = " + d2.Key + " = " + d.Value + "\n\n");
+}
---
+++ adventOfCode/day19/Scanner.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace day19;
+
+public class Scanner {
+    public int Number { get; set; }
+    public List<Vector3> Beacons { get; set; }
+
+    public Scanner(int number) {
+        Number = number;
+        Beacons = new List<Vector3>();
+    }
+}
