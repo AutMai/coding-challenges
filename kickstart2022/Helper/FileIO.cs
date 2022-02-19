f7c65f4d43f79c380be2af573d6ce2f930164f69
AutMai <shuber0802@gmail.com>
2022-02-19 19:03:55 +0100

    added kickstart2022

---
+++ kickstart2022/Helper/FileIO.cs
@@ -0,0 +1,13 @@
+﻿namespace Extensions;
+
+public class FileIO {
+    private static readonly string WorkingDirectory = Environment.CurrentDirectory;
+
+    public static string ReadFile(string filename) => File
+        .ReadAllText(Path.Combine(Directory.GetParent(WorkingDirectory).Parent.Parent.FullName, filename))
+        .Replace("\r", "");
+
+
+    public static IEnumerable<string> ReadLines(string filename) =>
+        File.ReadLines(Path.Combine(Directory.GetParent(WorkingDirectory).Parent.Parent.FullName, filename));
+}
---
+++ kickstart2022/Sample Problem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sample_Problem {
+    class Program {
+        static void Main(string[] args) {
+            var testCases = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= testCases; i++) {
+                var kingdom = Console.ReadLine();
+                
+                var name = ("aeiouAEIOU".IndexOf(kingdom.Last()) >= 0) ? "Alice" : "Bob";
+                if (kingdom.Last() == 'y') name = "nobody";
+
+                Console.WriteLine($"Case #{i}: {kingdom} is ruled by {name}.");
+            }
+        }
+    }
+}
