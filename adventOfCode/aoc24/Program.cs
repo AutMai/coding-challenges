d7d6da77300f72f361c4fb326c017c7be057fa8a
AutMai <shuber0802@gmail.com>
2024-12-03 21:29:07 +0100

    completed aoc24 day3

--- adventOfCode/aoc24/Program.cs
+++ adventOfCode/aoc24/Program.cs
@@ -1,4 +1,4 @@
-﻿using aoc24.day2;
-var day = new Day2();
+﻿using aoc24.day3;
+var day = new Day3();
 day.PuzzleOne();
 day.PuzzleTwo();
--- adventOfCode/aoc24/day3/Day3.cs
+++ adventOfCode/aoc24/day3/Day3.cs
@@ -1,4 +1,6 @@
-﻿using aocTools;
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using aocTools;
 
 namespace aoc24.day3;
 
@@ -7,64 +9,65 @@ public class Day3 : AAocDay {
         ReadInput();
     }
 
-    private List<List<int>> _reports = [];
+    private List<(int,int)> _multiplications = [];
 
-    private void ReadInput() {
-        _reports.Clear();
+    private void ReadInput(bool puzzleTwo = false) {
+        _multiplications.Clear();
+        var input = new StringBuilder();
         
         while (InputTokens.HasMoreTokens()) {
             var line = InputTokens.Read();
-            var split = line.Split(" ");
-            // convert to int
-            var levels = split.Select(int.Parse).ToList();
-            _reports.Add(levels);
+            input.Append(line);
         }
+        
+        if (puzzleTwo) {
+            input.Insert(0, "do()");
+            input.Append("don't()");
+            var inputStr = input.ToString();
+            var validBlocks = Regex.Matches(inputStr, "do\\(\\)(.*?)don't\\(\\)");
+            
+            foreach (Match validBlock in validBlocks) {
+                var matches = Regex.Matches(validBlock.Groups[1].Value, "mul\\((\\d{1,3}),(\\d{1,3})\\)");
+            
+                foreach (Match match in matches) {
+                    var x = int.Parse(match.Groups[1].Value);
+                    var y = int.Parse(match.Groups[2].Value);
+                    _multiplications.Add((x, y));
+                }
+            }
+        }
+        else {
+            var matches = Regex.Matches(input.ToString(), "mul\\((\\d{1,3}),(\\d{1,3})\\)");
+            
+            foreach (Match match in matches) {
+                var x = int.Parse(match.Groups[1].Value);
+                var y = int.Parse(match.Groups[2].Value);
+                _multiplications.Add((x, y));
+            }
+        }
+        
     }
 
     public override void PuzzleOne() {
-        var safeCount = _reports.Count(Safe);
-        Console.WriteLine(safeCount);
-    }
-
-    private static bool Safe(List<int> report) {
-        // if the list contains two times the same element => not safe
-        if (report.GroupBy(x => x).Any(g => g.Count() > 1)) {
-            return false;
+        var result = 0;
+        
+        foreach (var (x, y) in _multiplications) {
+            result += x * y;
         }
         
-        // if the list is sorted (either asc or desc) => safe
-        if (report.OrderBy(x => x).SequenceEqual(report) || report.OrderByDescending(x => x).SequenceEqual(report)) {
-            // max delta between each element and its neighbor is 2
-            var safe = true;
-            for (var i = 0; i < report.Count - 1; i++) {
-                if (Math.Abs(report[i] - report[i + 1]) > 3) {
-                    safe = false;
-                }
-            }
-
-            return safe;
-        }
-        
-        return false;
+        Console.WriteLine(result);
     }
-
-    private static bool AdvancedSafe(List<int> report) {
-        // remove 1 element and check if the list is safe
-        // try all possible combinations
-        for (var i = 0; i < report.Count; i++) {
-            var copy = new List<int>(report);
-            copy.RemoveAt(i);
-            if (Safe(copy)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-
 
     public override void PuzzleTwo() {
-        var safeCount = _reports.Count(AdvancedSafe);
-        Console.WriteLine(safeCount);
+        ResetInput();
+        ReadInput(true);
+        
+        var result = 0;
+        
+        foreach (var (x, y) in _multiplications) {
+            result += x * y;
+        }
+        
+        Console.WriteLine(result);
     }
 }
